d184cf36bf2d1adc8fa20132558b043abe09593b
konkit <konkit@gmail.com>
2013-05-10 16:36:58 +0200

    Commented it a little

--- ProjectSpaceshooter/Headers/GUISystem.h
+++ ProjectSpaceshooter/Headers/GUISystem.h
@@ -9,15 +9,18 @@ public:
 		ogre2dManager = new Ogre2dManager;
 		ogre2dManager->init(mSceneMgr, Ogre::RENDER_QUEUE_OVERLAY, true);
 
+		//This function loads image (in example from folder media/materials/textures/ )
 		//Ogre::TextureManager::getSingleton().load("Image.jpg",Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
 	}
 
 	void display(){
+		//This function displays this texture
 		//ogre2dManager->spriteBltFull("Image.jpg", -0.5, 0.5, 0.5, -0.5);
 	}
 
 
 	~GUISystem()	{
+		//This function unloads previously loaded texture
 		//Ogre::TextureManager::getSingleton().unload("Image.jpg");
 
 		ogre2dManager->end();
