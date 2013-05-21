bc1d4358f01c49c4508cac0d8e5c24dbed381e4e
konkit <konkit@gmail.com>
2013-05-21 14:23:42 +0200

    GUI loaded from file

--- ProjectSpaceshooter/Headers/GUISystem.h
+++ ProjectSpaceshooter/Headers/GUISystem.h
@@ -32,68 +32,51 @@ public:
 			mRadar.init(ogre2dManager);
 
 		//Text Stuff
-			loadFont("arial.ttf");
+			//Load Font
+			Ogre::FontManager &fontMgr = Ogre::FontManager::getSingleton();
+			Ogre::ResourcePtr font = fontMgr.getByName("MyFont");
+			font->load();
 
-			// get the resource manager
-			overlayManager = & Ogre::OverlayManager::getSingleton();
+			//Get overlay object
+			Ogre::OverlayManager* overlayManager = & Ogre::OverlayManager::getSingleton();
+			overlay = overlayManager->getByName("GUIOverlay");
 
-			// Create a player data panel
-			playerDataPanel = static_cast<Ogre::OverlayContainer*>(
-				overlayManager->createOverlayElement("Panel", "PanelName1"));
-			playerDataPanel->setMetricsMode(Ogre::GMM_PIXELS);
-			playerDataPanel->setPosition(10, 20);
-			playerDataPanel->setDimensions(200, 120);
-			playerDataPanel->setMaterialName( "grayBackground" );
-
-			// Create a message log panel
-			messageLogPanel = static_cast<Ogre::OverlayContainer*>(
-				overlayManager->createOverlayElement("Panel", "PanelName2"));
-			messageLogPanel->setMetricsMode(Ogre::GMM_PIXELS);
-			messageLogPanel->setPosition(10, 200);
-			messageLogPanel->setDimensions(200, 120);
-			messageLogPanel->setMaterialName( "grayBackground" );
-
-			// Create an overlay, and add the panel
-			overlay = overlayManager->create("OverlayName");
-			overlay->add2D(playerDataPanel);
-			overlay->add2D(messageLogPanel);
-
-
-
-			//create text areas
-			playerData = createText(playerDataPanel, "playerVelocity1", 0, 5 );
-			mMessageConsole =  createText(messageLogPanel, "messageLog1", 0, 5 );
-		//END text stuff
+			//Get text area data to modify it later
+			Ogre::OverlayContainer* playerDataPanel = static_cast<Ogre::OverlayContainer*>( overlayManager->getOverlayElement("playerDataPanel") );
+			playerData = playerDataPanel->getChild("PlayerDataText");
+			
+			Ogre::OverlayContainer* messageLogPanel = static_cast<Ogre::OverlayContainer*>( overlayManager->getOverlayElement("messageLogPanel") );
+			mMessageConsole = messageLogPanel->getChild("MessageLogText");
 	}
 
 	//display - run every frame
 	void display( GameData& mGameData, TimeData _time ){
 
 		//Radar
-		mRadar.display(mGameData, _time);			
+			mRadar.display(mGameData, _time);			
 
 		//Text stuff
 
-		//display player velocity as text (actualize text contents)
-		std::stringstream playerDataString;
-			playerDataString
-				<<"Velocity : "<<mGameData.getPlayer()->getPhysicsComponent().getCurrentVelocityMagnitude()
-				<<"\nTarget velocity : "<<mGameData.getPlayer()->getPhysicsComponent().getTargetVelocityValue()
-				<<"\nHealth : "<<mGameData.getPlayer()->getHealthValue()<<" / "<<mGameData.getPlayer()->getMaxHealthValue()
-				<<"\n"
-				<<"\nFPS : "<<_time.currentFPSValue;
-		playerData->setCaption( playerDataString.str() );
+			//display player velocity as text (actualize text contents)
+			std::stringstream playerDataString;
+				playerDataString
+					<<"Velocity : "<<mGameData.getPlayer()->getPhysicsComponent().getCurrentVelocityMagnitude()
+					<<"\nTarget velocity : "<<mGameData.getPlayer()->getPhysicsComponent().getTargetVelocityValue()
+					<<"\nHealth : "<<mGameData.getPlayer()->getHealthValue()<<" / "<<mGameData.getPlayer()->getMaxHealthValue()
+					<<"\n"
+					<<"\nFPS : "<<_time.currentFPSValue;
+			playerData->setCaption( playerDataString.str() );
 
-		//draw message console
-		mConsole = &mGameData.getMessageConsole();
-		std::stringstream messageConsoleString;
-			messageConsoleString
-				<<"Log :\n"
-				<<mConsole->getMessages();
-		mMessageConsole->setCaption( messageConsoleString.str() );
+			//draw message console
+			mConsole = &mGameData.getMessageConsole();
+			std::stringstream messageConsoleString;
+				messageConsoleString
+					<<"Log :\n"
+					<<mConsole->getMessages();
+			mMessageConsole->setCaption( messageConsoleString.str() );
 
 		// Show the overlay
-		overlay->show();
+			overlay->show();
 	}
 
 
@@ -110,57 +93,14 @@ private:
 
 	//My own overlay play
 	Ogre::Overlay* overlay;
-	Ogre::OverlayManager* overlayManager;
+	
 
-	//Panels
-	Ogre::OverlayContainer* playerDataPanel;
-	Ogre::OverlayContainer* messageLogPanel;
-
-	Ogre::TextAreaOverlayElement* playerData;
-	Ogre::TextAreaOverlayElement* mMessageConsole;
+	//Texts
+	Ogre::OverlayElement* playerData;
+	Ogre::OverlayElement* mMessageConsole;
 
 	Radar mRadar;
 	MessageConsole* mConsole;
-
-	//load font
-	void loadFont(std::string fontFilename)	{
-		Ogre::ResourceGroupManager &resGroupMgr = Ogre::ResourceGroupManager::getSingleton();
-			// tell it to look at this location
-			resGroupMgr.addResourceLocation(FONT_FOLDER, "FileSystem");
-			// get the font manager
-			Ogre::FontManager &fontMgr = Ogre::FontManager::getSingleton();
-			// create a font resource
-			Ogre::ResourcePtr font = fontMgr.create("MyFont", "General");
-			// set as truetype
-			font->setParameter("type", "truetype");
-			// set the .ttf file name
-			font->setParameter("source", fontFilename);
-			// set the size
-			font->setParameter("size", "26");
-			// set the dpi
-			font->setParameter("resolution", "96");
-			// load the ttf
-			font->load();
-	}
-
-	//create text area object, attach it to panel and return it (so it can be saved and have the text actualized)
-	Ogre::TextAreaOverlayElement* createText(Ogre::OverlayContainer* panel, std::string elementID, int posX, int posY )	{
-		// Create a text area
-		Ogre::TextAreaOverlayElement* textArea = static_cast<Ogre::TextAreaOverlayElement*>(
-			overlayManager->createOverlayElement("TextArea", elementID));
-		textArea->setMetricsMode(Ogre::GMM_PIXELS);
-		textArea->setPosition(posX, posY);
-		textArea->setDimensions(100, 20);
-		textArea->setCharHeight(16);
-		textArea->setFontName("MyFont");
-		textArea->setColourBottom(Ogre::ColourValue(0.3, 0.5, 0.3));
-		textArea->setColourTop(Ogre::ColourValue(0.5, 0.7, 0.5));
- 
-		// Add the text area to the panel
-		panel->addChild(textArea);
-
-		return textArea;
-	}
 };
 
 
